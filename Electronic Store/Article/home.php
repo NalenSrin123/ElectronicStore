4cc1317d2c4c881016034cd442e4b641944e37c1
Nalen Srin <161912473+NalenSrin123@users.noreply.github.com>
2024-12-09 18:56:28 +0700

    Add files via upload

---
+++ Electronic Store/Article/home.php
@@ -0,0 +1,74 @@
+<div class="box-card">
+    <div class="cards">
+            <div class="title">
+                Lorem ipsum dolor sit amet.
+            </div>
+            <div class="image">
+                <img src="https://econtent.o2.co.uk/o/econtent/media/get/248e46a7-5a82-492f-9934-d483f0a416ae" alt="">
+            </div>
+            <div class="price">
+                <span>price : 1999$</span>
+                <button >Add to cart</button>
+            </div>
+        </div>
+    <div class="cards">
+            <div class="title">
+                Lorem ipsum dolor sit amet.
+            </div>
+            <div class="image">
+                <img src="https://econtent.o2.co.uk/o/econtent/media/get/248e46a7-5a82-492f-9934-d483f0a416ae" alt="">
+            </div>
+            <div class="price">
+                <span>price : 1999$</span>
+                <button >Add to cart</button>
+            </div>
+        </div>
+    <div class="cards">
+            <div class="title">
+                Lorem ipsum dolor sit amet.
+            </div>
+            <div class="image">
+                <img src="https://econtent.o2.co.uk/o/econtent/media/get/248e46a7-5a82-492f-9934-d483f0a416ae" alt="">
+            </div>
+            <div class="price">
+                <span>price : 1999$</span>
+                <button >Add to cart</button>
+            </div>
+        </div>
+    <div class="cards">
+            <div class="title">
+                Lorem ipsum dolor sit amet.
+            </div>
+            <div class="image">
+                <img src="https://econtent.o2.co.uk/o/econtent/media/get/248e46a7-5a82-492f-9934-d483f0a416ae" alt="">
+            </div>
+            <div class="price">
+                <span>price : 1999$</span>
+                <button >Add to cart</button>
+            </div>
+        </div>
+    <div class="cards">
+            <div class="title">
+                Lorem ipsum dolor sit amet.
+            </div>
+            <div class="image">
+                <img src="https://econtent.o2.co.uk/o/econtent/media/get/248e46a7-5a82-492f-9934-d483f0a416ae" alt="">
+            </div>
+            <div class="price">
+                <span>price : 1999$</span>
+                <button >Add to cart</button>
+            </div>
+        </div>
+    <div class="cards">
+            <div class="title">
+                Lorem ipsum dolor sit amet.
+            </div>
+            <div class="image">
+                <img src="https://econtent.o2.co.uk/o/econtent/media/get/248e46a7-5a82-492f-9934-d483f0a416ae" alt="">
+            </div>
+            <div class="price">
+                <span>price : 1999$</span>
+                <button >Add to cart</button>
+            </div>
+        </div>
+    </div>
